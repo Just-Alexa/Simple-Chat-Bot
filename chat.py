b1533b4345f52d02acc41a90ddc4748a902492fd
Alexa <onabanjotomiwa@gmail.com>
2023-09-12 17:33:52 +0100

    Chat function

--- chat.py
+++ chat.py
@@ -1,37 +1,44 @@
 import random
 
-# Define lists of greetings, questions, and responses
-greetings = ["Hello!", "Hi there!", "Hey!", "Greetings!"]
-questions = ["How are you today?", "What's new in your world?", "Anything exciting happening?"]
-responses = [
-    "That's interesting.",
-    "Tell me more about that.",
-    "I see. How do you feel about it?",
-    "Interesting! How did that make you feel?",
-]
+class ChatBot:
+    def __init__(self):
+        self.responses = {
+            "hello": ["Hello!", "Hi there!", "Hey!", "wats up!" "Greetings!"],
+            "I'm doing well": ["That's good to hear!", "Awesome!", "Glad to hear it!"],
+            "I'm great": ["That's good to hear!", "Awesome!", "Glad to hear it!"],
+            "I'm good": ["That's good to hear!", "Awesome!", "Glad to hear it!"],
+            "good": ["That's good to hear!", "Awesome!", "Glad to hear it!"],
+            "fine": ["That's good to hear!", "Awesome!", "Glad to hear it!"],
+            "what are you doing": ["Waiting on your command!", "I'm just hanging out.", "I'm here to chat with you!"],
+            "what's up": ["Not much!", "The sky!"],
+            "how's your day going": ["Great!", "Not too bad.", "Just another day in paradise."],
+            "how are you": ["I'm doing well, thanks!", "I'm just a computer program, but I'm here to chat with you!"],
+            "tell me a joke": ["Why did the computer catch a cold? It had too many windows open!", "Here's one: What do you call a computer that sings? A-Dell!"],
+            "goodbye": ["Goodbye!", "Have a great day!"],
+            "bye": ["Catch you later, aligator!"],
+        }
 
-# Function to generate a random response
-def get_random_response(options):
-    return random.choice(options)
-    
-# Function to handle user input and generate responses
-def chat(bot_name):
-    print(f"{bot_name}: " + get_random_response(greetings))
-    while True:
-        user_input = input("You: ")
-        if user_input.lower() in ['exit', 'quit', 'bye', 'goodbye']:
-            print(f"{bot_name}: Goodbye! Have a great day.")
-            break
-        elif 'how are you' in user_input.lower():
-            print(f"{bot_name}: I'm just a computer program, so I don't have feelings, but I'm here to chat with you!")
-        else:
-            response = get_random_response(responses)
-            print(f"{bot_name}: " + response)
+    def get_response(self, user_input):
+        user_input = user_input.lower()
+        for keyword, responses in self.responses.items():
+            if keyword in user_input:
+                return random.choice(responses)
+        return "I'm not sure how to respond to that."
+
+    def chat_with_user(self):
+        print("ChatBot: Hello! How can I assist you today?")
+        while True:
+            user_input = input("You: ")
+            if user_input.lower() in ['exit', 'quit', 'bye', 'goodbye']:
+                print("ChatBot: Goodbye! Have a great day.")
+                break
+            response = self.get_response(user_input)
+            print(f"ChatBot: {response}")
 
 def main():
-    bot_name = "Chatty"  # Specify the chatbot's name here
-    print(f"{bot_name}: Hello! I'm a {bot_name}. You can start a conversation with me. Type 'exit' to end the chat.")
-    chat(bot_name)
+    print("Welcome to the ChatBot!")
+    chatbot = ChatBot()
+    chatbot.chat_with_user()
 
 if __name__ == "__main__":
     main()
